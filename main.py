6f02980f8123ca772b677955444b77274467bac3
prabhatpps <prabhatpandeypps@gmail.com>
2023-05-25 22:05:55 +0530

    new

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
 import Question_Frame
 import Addition_Table
 import Calculation
-
-# Fix the given bug
-# USA + USSR = PEACE
-# p = 1, E = 0, U = 9, A = 2, R = 8, S = 3, c = 7
-
-
-# add multi element addition
-# Subtraction
